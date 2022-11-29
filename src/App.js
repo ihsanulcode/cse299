87a2d77ac4b2468f051103f2a13e376d884fde9f
Ihsanul Haque Asif <66785935+ihsanulcode@users.noreply.github.com>
2022-11-29 12:55:33 +0600

    set react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layouts/Main";
+import Login from "./Pages/Login/Login";
+import Registration from "./Pages/Registration/Registration";
+import Home from "./Pages/Home/Home";
+import ProductExtendedView from "./Pages/ProductExtendedView/ProductExtendedView";
+import SearchResult from "./Pages/SearchResult/SearchResult";
+import Categories from "./components/Header/HeaderBody/Categories/Categories";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "/login",
+          element: <Login></Login>,
+        },
+        {
+          path: "/register",
+          element: <Registration></Registration>,
+        },
+        {
+          path: "/productExtendedView/:id",
+          loader: ({ params }) =>
+            fetch(`https://machbazar-server.vercel.app/fishCard/${params.id}`),
+          element: <ProductExtendedView></ProductExtendedView>,
+        },
+        {
+          path: `/categories/:id`,
+          element: <SearchResult></SearchResult>,
+          loader: ({ params }) =>
+            fetch(
+              `https://machbazar-server.vercel.app/categories/${params.id}`
+            ),
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
